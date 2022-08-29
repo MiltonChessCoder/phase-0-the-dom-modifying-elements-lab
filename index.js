7e19c88b0301ffeb3d892ae6007231838eeab400
Milton <miltonsoftwareengineer@gmail.com>
2022-08-29 12:53:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+const changemain = document.getElementById('main')
+changemain.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent = 'Milton is the champion'
+
